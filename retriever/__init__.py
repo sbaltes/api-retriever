655481db75dbc49a9cea3b703f1e541eda2e3e7b
Sebastian Baltes <research@sbaltes.com>
2018-06-07 20:15:21 +0200

    Adjust delay and rename default log file

--- retriever/__init__.py
+++ retriever/__init__.py
@@ -1,6 +1,6 @@
 import util.log
 
-LOG_FILE = 'api-retriever-retriever.log'
+LOG_FILE = 'api-retriever.log'
 
 # initialize named global logger
 logger = util.log.configure_logger('api-retriever_logger', LOG_FILE)
